22804aa0084eefc655f5409f06c78977b487db76
loup-sardin <loupsardin@gmail.com>
2024-11-05 15:21:38 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -3,18 +3,18 @@ const config = {
   description: {
     EN: {
       short: `Loup Sardin is a doctoral student <a href="https://umr-lisis.fr/membre/loup-sardin/" target="_blank">
-     affiliated to the Laboratoire Interdisciplinaire Sciences Innovations Sociétés (LISIS) grant</a> and Paris Saclay Applied Economics (PSAE) since April 2023, 
+      affiliated to the Laboratoire Interdisciplinaire Sciences Innovations Sociétés (LISIS) grant</a> and Paris Saclay Applied Economics (PSAE) since April 2023, 
       where he is working on a PHD on the integration of environmental imperatives in companies, 
       taking as a case study the management and potential valorisation of vegetation at SNCF. 
       His research is being supervised by Emmanuel Kessous (LISIS) and Caroline Orset (PSAE). 
-      This CIFRE PHD, in sociology and economics, is financially supported by SNCF Réseau.
+      This CIFRE PHD, in sociology and economics, is financially supported by SNCF Réseau.`,
       long: "",
     },
     FR: {
       short: `Loup Sardin est doctorant au <a href="https://umr-lisis.fr/membre/loup-sardin/" target="_blank">Laboratoire Interdisciplinaire
-          Sciences Innovations Sociétés (LISIS) grant</a> et à Paris Saclay Applied Economics (PSAE) depuis avril 2023
-          où il réalise une thèse sur l’intégration des impératifs environnementaux en entreprise, en prenant pour étude de cas la maîtrise et la valorisation potentielle de la végétation à la SNCF. 
-          Sa recherche est dirigée par Emmanuel Kessous (LISIS) et Caroline Orset (PSAE). Cette thèse CIFRE, en sociologie et économie, bénéficie du soutien financier de SNCF Réseau.  `,
+      Sciences Innovations Sociétés (LISIS) grant</a> et à Paris Saclay Applied Economics (PSAE) depuis avril 2023
+      où il réalise une thèse sur l’intégration des impératifs environnementaux en entreprise, en prenant pour étude de cas la maîtrise et la valorisation potentielle de la végétation à la SNCF. 
+      Sa recherche est dirigée par Emmanuel Kessous (LISIS) et Caroline Orset (PSAE). Cette thèse CIFRE, en sociologie et économie, bénéficie du soutien financier de SNCF Réseau.`,
       long: "",
     },
   },
